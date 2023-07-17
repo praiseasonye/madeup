86d0acb9b271dac334f341d18ebf07b4899cef39
praiseasonye <praiseikennaasonye@gmail.com>
2023-07-17 20:13:27 +0000

    header file added

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+#endif /*main.h*/
+
