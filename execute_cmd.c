75d0781124d3f26a4eadb7a4a7624dd7f9306126
praiseasonye <praiseikennaasonye@gmail.com>
2023-07-19 17:45:33 +0000

    shell rceives and executes command with path passed to it as a command

---
+++ execute_cmd.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * execute_cmd - a function that executes a shell command
+ *
+ * @argv: a pointer to a string (the command line argument
+ *        to execute
+ *
+ * Return: void
+ *
+ */
+
+void execute_cmd(char **argv)
+{
+	char *cmd;
+	int exec_return;
+
+	if (argv)
+	{
+		cmd = argv[0];
+		
+		exec_return = execve(cmd, argv, NULL);
+		if (exec_return == -1)
+			write(STDERR_FILENO, "NOT FOUND: No such file or directory", 36);
+	}
+}
---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+void execute_cmd(char **argv);
+#endif /* main.h */
