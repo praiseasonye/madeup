4691b422d66b5e54da0511fe81f52043ab77fa22
praiseasonye <praiseikennaasonye@gmail.com>
2023-07-21 15:46:53 +0000

    the shell can now tak arguments, create a child process and then execute

--- execute_cmd.c
+++ execute_cmd.c
@@ -14,13 +14,35 @@ void execute_cmd(char **argv)
 {
 	char *cmd;
 	int exec_return;
+	pid_t child_pid;
+	int status;
 
 	if (argv)
 	{
-		cmd = get_cmddir(argv[0]);
-		
-		exec_return = execve(cmd, argv, NULL);
-		if (exec_return == -1)
-			write(STDERR_FILENO, "NOT FOUND: No such file or directory\n", 37);
+		child_pid = fork();
+		if (child_pid < 0)
+		{
+			write(STDERR_FILENO, "fork process failed", 19);
+			return;
+		}
+		else if (child_pid == 0)
+		{
+			cmd = get_cmddir(argv[0]);
+			if (cmd == NULL)
+			{
+				write(STDERR_FILENO, "Command not found\n", 18);
+				exit(EXIT_FAILURE);
+			}
+			exec_return = execve(cmd, argv, NULL);
+			if (exec_return == -1)
+			{
+				write(STDERR_FILENO, "NOT FOUND: No such file or directory\n", 37);
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		else
+			waitpid(child_pid, &status, 0);
 	}
+
 }
