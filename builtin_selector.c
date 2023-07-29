7947deeff367dabbfd6c42c420762e021a4b1753
praiseasonye <praiseikennaasonye@gmail.com>
2023-07-29 03:38:02 +0000

    refactorrd token maker code

--- builtin_selector.c
+++ builtin_selector.c
@@ -1,33 +1,168 @@
 #include "main.h"
 
 /**
- * builtin_selector - a function that returns a pointer to a
- *                    builtin shell function.
+ * hsh - main shell loop
  *
- * @str: the command that determines what builtin function to return
+ * @info: the parametr & return info struct
  *
- * Return: a poinetr to a function
+ * @av: the argument vector from main()
  *
+ * Return: 0 on success, 1 on error or error code
  */
 
-void (*builtin_selector(char *str))(char *str)
+int hsh(ShellInfo  *info, char **av)
 {
-	int i;
+	ssize_t r = 0;
+	int builtin_ret = 0;
 
-	builtin_t builtins[] = {
-		{"exit", exit_b},
-		{"env", env_b},
-		{"cd", cd_b},
+	while (r != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("#cisfun$ ");
+		eputchar(BUFF_FLUSH);
+		r = getinput(info);
+		if (r != -1)
+		{
+			setinfo(info, av);
+			builtin_ret = findbuiltin(info);
+			if (builtin_ret == -1)
+				findcmd(info);
+		}
+		else if (interactive(info))
+			putchar_('\n');
+		freeinfo(info, 0);
+	}
+	writehistory(info);
+	freeinfo(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
+}
+
+/**
+ * find_builtin - find a builtin command
+ * @info: the paramet & return info struct
+ *
+ * Return: -1 if builtin not found,
+ * 0 if builtin executed successfully,
+ * 1 if builtin found but not successful,
+ * -2 if builtin signals exit()
+ */
+int find_builtin(ShellInfo  *info)
+{
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", myexit},
+		{"env", myenv},
+		{"help",myhelp},
+		{"history", myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", myunsetenv},
+		{"cd", mycd},
+		{"alias", myalias},
 		{NULL, NULL}
 	};
 
-		/* check if the command is a built-in command*/
-		for (i = 0; builtins[i].name != NULL; i++)
+	for (i = 0; builtintbl[i].type; i++)
+		if (strcmp_(info->argv[0], builtintbl[i].type) == 0)
 		{
-			if (_strcmp(str, builtins[i].name) == 0)
-			{
-				return (builtins[i].f);
-			}
+			info->line_count++;
+			built_in_ret = builtintbl[i].func(info);
+			break;
 		}
-		return (NULL);
+	return (built_in_ret);
+}
+
+
+/**
+ * find_cmd - finds a command in PAT
+ *
+ * @info: the parameter & return info struct
+ *
+ * Return: void
+ */
+
+void find_cmd(ShellInfo  *info)
+{
+	char *path = NULL;
+	int i, k;
+
+	info->path = info->argv[0];
+	if (info->linecount_flag == 1)
+	{
+		info->line_count++;
+		info->linecount_flag = 0;
+	}
+	for (i = 0, k = 0; info->arg[i]; i++)
+		if (!isdelim(info->arg[i], " \t\n"))
+			k++;
+	if (!k)
+		return;
+	path = find_path(info, getenv_(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		fork_cmd(info);
+	}
+	else
+	{
+		if ((interactive(info) || getenv_(info, "PATH=")
+			|| info->argv[0][0] == '/') && i_cmd(info, info->argv[0]))
+			forkcmd(info);
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_error(info, "not found\n");
+		}
+	}
+}
+
+
+/**
+ * fork_cmd - forks an exec thread to run cmd
+ *
+ * @info: the parameter & return info struct
+ *
+ * Return:
+ * void
+ */
+void forkcmd(ShellInfo  *info)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		/* TODO: PUT ERROR FUNCTION */
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, getenviron(info)) == -1)
+		{
+			freeinfo(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+		/* TODO: PUT ERROR FUNCTION */
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
+	}
 }
---
+++ variable.c
@@ -0,0 +1,169 @@
+#include "shell.h"
+
+/**
+ * ischain - test if current char in buffer is a chain delimeter
+ *
+ * @info: the parater struct
+ *
+ * @buf: the char buffer
+ *
+ * @p: address of current position in buf
+ *
+ * Return: 1 if chain delimeter, 0 otherwise
+ *
+ */
+
+int ischain(Shell *info, char *buf, size_t *p)
+{
+	size_t j = *p;
+
+	if (buf[j] == '|' && buf[j + 1] == '|')
+	{
+		buf[j] = 0;
+		j++;
+		info->cmd_buf_type = CMDOR;
+	}
+	else if (buf[j] == '&' && buf[j + 1] == '&')
+	{
+		buf[j] = 0;
+		j++;
+		info->cmd_buf_type = CMDAND;
+	}
+	else if (buf[j] == ';')
+	{
+		buf[j] = 0; 
+		info->cmd_buf_type = CMDCHAIN;
+	}
+	else
+		return (0);
+	*p = j;
+	return (1);
+}
+
+/**
+ * checkchain - checks we should continue chaining based on last status
+ *
+ * @info: the parameter struct
+ *
+ * @buf: the char buffer
+ *
+ * @p: address of current position in buf
+ *
+ * @i: starting position in buf
+ *
+ * @len: length of buf
+ *
+ * Return: void
+ */
+
+void check_chain(Shell *info, char *buf, size_t *p, size_t i, size_t len)
+{
+	size_t j = *p;
+
+	if (info->cmd_buf_type == CMDAND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	if (info->cmd_buf_type == CMDOR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			j = len;
+		}
+	}
+	*p = j;
+}
+
+/**
+ * replacealias - replaces an aliases in the tokenized string
+ *
+ * @info: the parameter struct
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replacealias(Shell  *info)
+{
+	int i;
+	list_t *node;
+	char *p;
+
+	for (i = 0; i < 10; i++)
+	{
+		node = nodestartswith(info->alias, info->argv[0], '=');
+		if (!node)
+			return (0);
+		free(info->argv[0]);
+		p = strchr_(node->str, '=');
+		if (!p)
+			return (0);
+		p = strdup_(p + 1);
+		if (!p)
+			return (0);
+		info->argv[0] = p;
+	}
+	return (1);
+}
+
+/**
+ * replace_vars - replaces vars in the tokenized string
+ * @info: the parameter struct
+ *
+ * Return: 1 if replaced, 0 otherwise
+ */
+int replacevars(Shell  *info)
+{
+	int i = 0;
+	list_t *node;
+
+	for (i = 0; info->argv[i]; i++)
+	{
+		if (info->argv[i][0] != '$' || !info->argv[i][1])
+			continue;
+
+		if (!strcmp_(info->argv[i], "$?"))
+		{
+			replacestring(&(info->argv[i]),
+				strdup_(convertnumber(info->status, 10, 0)));
+			continue;
+		}
+		if (!strcmp_(info->argv[i], "$$"))
+		{
+			replacestring(&(info->argv[i]),
+					strdup_(convertnumber(getpid(), 10, 0)));
+			continue;
+		}
+		node = nodestartswith(info->env, &info->argv[i][1], '=');
+		if (node)
+		{
+			replacestring(&(info->argv[i]),
+				strdup_(strchr_(node->str, '=') + 1));
+			continue;
+		}
+		replacestring(&info->argv[i], _strdup(""));
+
+	}
+	return (0);
+}
+
+/**
+ * replace_string - replaces string - replaces string
+ *
+ * @old: address of old string
+ *
+ * @new: new string
+ *
+ * Return: 1 if replaced, 0 otherwise
+ *
+ */
+
+int replacestring(char **old, char *new)
+{
+	free(*old);
+	*old = new;
+	return (1);
+}
